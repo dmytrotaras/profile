115350e079b056387f7c43d249f721947044d30a
dmytro5153 <dmytro5153@gmail.com>
2023-02-17 14:35:53 +0100

    changes to layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
         <ul className='permalinks'>
             <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="#hobby">Testimonials</a></li>
+            <li><a href="#experience">Skills</a></li>
+            <li><a href="#education">Education</a></li>
+            <li><a href="#portfolio">Experience</a></li>
+            <li><a href="#hobbyslider">Hobby</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
         <div className="footer__socials">
